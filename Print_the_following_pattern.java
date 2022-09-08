22efd1ebd7aa1d248d404a031a10591ec1ab04b3
RaveeTeza <84396726+RaveeTeza@users.noreply.github.com>
2022-09-08 20:51:55 +0530

    import java.util.*; class pattern1234 {    public static void main(String args[])     // To do auto generated method stub {   Scanner sc=new Scanner (System.in); int r; r=sc.nextInt(); for(int i=1;i<=r; i++) {    for (int j=1;j<=r;j++) {     if (i<=j) System.out.print(j+" "); }   System.out.println(); } } }

---
+++ Print_the_following_pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern1234
+{
+   public static void main(String args[])     // To do auto generated method stub
+{
+  Scanner sc=new Scanner (System.in);
+int r;
+r=sc.nextInt();
+for(int i=1;i<=r; i++)
+{
+   for (int j=1;j<=r;j++)
+{
+    if (i<=j)
+System.out.print(j+" ");
+}
+  System.out.println();
+}
+}
+}
