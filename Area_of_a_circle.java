7eb0b6c9b02ad2a113c4fb57552ed22029cae7dd
RaveeTeza <84396726+RaveeTeza@users.noreply.github.com>
2022-11-05 00:47:24 +0530

    import java.util.*; class area {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         double area=(n*n)*3.14;         System.out.println(area);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class area
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        double area=(n*n)*3.14;
+        System.out.println(area);
+    }
+}
